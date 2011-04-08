0e3e59d290d64825d53d68d530537fd33bd6deaf
Ionuț Arțăriși <mapleoin@lavabit.com>
2011-04-08 16:14:34 +0200

    extract more information and display it

--- server/popcorn.py
+++ server/popcorn.py
@@ -34,12 +34,18 @@ rdb = redis.Redis(host='localhost', port=6379, db=0)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        """Front page"""
         hw_uuids = rdb.lrange('hw-uuids', 0, -1)
 
         total_packages = rdb.get('global:nextPackageId')
 
         names, voted, recent, old, nofiles = get_sorted_packages()
+        vendors = rdb.smembers('vendors')
+        vendor_keys = ['vendor:%s' % i for i in vendors]
+        vendor_ids = rdb.mget(vendor_keys)
         self.render('index.html',
+                    vendors=vendors,
+                    vendor_ids=vendor_ids,
                     systems=hw_uuids,
                     total_packages=total_packages,
                     names=names,
@@ -48,20 +54,10 @@ class MainHandler(tornado.web.RequestHandler):
                     old=old,
                     nofiles=nofiles)
 
-    def post(self):
-        self.set_header("Content-Type", "text/plain")
-        try:
-            assert self.request.files['popcorn'][0][
-                'content_type'] == 'text/plain', tornado.web.HTTPError(415)
-        except KeyError:
-            raise Exception("nothing uploaded")
-
-        parse_popcorn(self.request.files['popcorn'][0]['body'])
-
-        self.write('Submission received. Thank you!')
 
 class SystemHandler(tornado.web.RequestHandler):
     def get(self, hw_uuid):
+        """Display information about a system indentified by its 'hw_uuid'"""
         try:
             system_id = rdb['hw-uuid:%s' % hw_uuid]
         except KeyError:
@@ -75,8 +71,10 @@ class SystemHandler(tornado.web.RequestHandler):
         for sub in subs:
             self.write("<a href='/submission/%s'>submission %s</a>" % (sub, sub))
 
+
 class SubmissionHandler(tornado.web.RequestHandler):
     def get(self, sub_id):
+        """Display a submission identified by 'sub_id'"""
         package_ids = rdb.lrange('submission:%s:packages' % sub_id, 0, -1)
         name_keys = ['package:%s:name' % i for i in package_ids]
         package_names = rdb.mget(name_keys)
@@ -85,6 +83,36 @@ class SubmissionHandler(tornado.web.RequestHandler):
         for name in package_names:
             self.write('<li>%s</li>' % name)
         self.write('</ul>')
+
+    def post(self):
+        """Accepts submissions from popcorn clients in plain text"""
+        self.set_header("Content-Type", "text/plain")
+        try:
+            assert self.request.files['popcorn'][0][
+                'content_type'] == 'text/plain', tornado.web.HTTPError(415)
+        except KeyError:
+            raise Exception("nothing uploaded")
+
+        parse_popcorn(self.request.files['popcorn'][0]['body'])
+
+        self.write('Submission received. Thank you!')
+
+
+class VendorHandler(tornado.web.RequestHandler):
+    def get(self, vendor_id):
+        """Display information about a specific vendor"""
+        vendor_name = rdb['vendor:%s' % vendor_id]
+        packages = rdb.smembers('vendor:%s:packages' % vendor_id)
+        if not packages:
+            raise tornado.web.HTTPError(
+                404, 'Could not find vendor identified by %s' % vendor_id)
+        self.write('<p>Packages provided by %s:</p>' % vendor_name)
+        self.write('<ul>')
+        for package in packages:
+            self.write('<li>%s</li>' % rdb.get('package:%s:name' % package))
+        self.write('</ul>')
+
+
 def get_sorted_packages(key='voted'):
     """Get a lists of package attributes sorted by `key`
 
@@ -119,16 +147,17 @@ def get_system_id(hw_uuid):
         rdb[key] = system_id
     return system_id
 
-def get_package_id(package_name):
-    """Get or create a package_id for this package_name"""
-    key = 'packageName:%s' % package_name
+def get_package_id(name, ver, rel, arch, vendor):
+    """Get or create a package for this name, ver, rel, arch, vendor combo"""
+    key = 'packageId:%(name)s:%(ver)s:%(rel)s:%(arch)s:%(vendor)s' % locals()
     try:
         package_id = rdb[key]
     except KeyError:
         package_id = str(rdb.incr('global:nextPackageId'))
         rdb[key] = package_id
         rdb.lpush('packageIds', package_id)
-        rdb.set('package:%s:name' % package_id, package_name)
+        rdb.set('package:%s:name' % package_id, name)
+        rdb.sadd('vendor:%s:packages' % vendor, package_id)
     return package_id
 
 def get_submission(system_id):
@@ -142,6 +171,22 @@ def get_submission(system_id):
     rdb.lpush('system:%s:submissions' % system_id, sub_id)
 
     return sub_id
+
+def normalize_vendor(vendor_name):
+    # redis keys cannot contain spaces
+    return vendor_name.replace(' ', '_')
+
+def get_vendor_id(name):
+    """Get or create a new vendor"""
+    name = normalize_vendor(name)
+    try:
+        vendor_id = rdb['vendor:%s' % name]
+    except KeyError:
+        vendor_id = str(rdb.incr('global:nextVendorId'))
+        rdb.set('vendor:%s' % name, vendor_id)
+        rdb.set('vendor:%s' % vendor_id, name)
+        rdb.sadd('vendors', name)
+    return vendor_id
     
 def parse_popcorn(data):
     datalines = data.splitlines()
@@ -153,8 +198,9 @@ def parse_popcorn(data):
     sub = get_submission(system_id)
     rdb.set('submission:%s:popcorn' % sub, version)
     for line in datalines[1:]:
-        (status, package_name) = line.split()
-        package_id = get_package_id(package_name)
+        (status, name, version, release, arch, vendor) = line.split(None, 5)
+        vendor_id = get_vendor_id(vendor)
+        package_id = get_package_id(name, version, release, arch, vendor_id)
         if status == 'v':
             rdb.incr('package:%s:voted' % package_id)
         elif status == 'r':
@@ -165,11 +211,12 @@ def parse_popcorn(data):
             rdb.incr('package:%s:nofiles' % package_id)
         # add package to the submission
         rdb.lpush('submission:%s:packages' % sub, package_id)
-        
+
 application = tornado.web.Application([
-    (r"/post", MainHandler),
-    (r"/system/([a-f0-9\-]+)", SystemHandler),
-    (r"/submission/([0-9]+)", SubmissionHandler),
+        (r"/", MainHandler),
+        (r"/system/([a-f0-9\-]+)", SystemHandler),
+        (r"/submission/([0-9]+)?", SubmissionHandler),
+        (r"/vendor/([0-9]+)", VendorHandler),
 ])
 
 if __name__ == "__main__":
