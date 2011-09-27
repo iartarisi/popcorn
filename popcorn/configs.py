4b2bca0798662ed490fa6942d9211cc39eafb16e
Ionuț Arțăriși <iartarisi@suse.cz>
2011-09-27 13:31:54 +0200

    make config a "public" object

--- popcorn/configs.py
+++ popcorn/configs.py
@@ -4,8 +4,8 @@ from ConfigParser import RawConfigParser
 
 CONFIG_FILE = '/etc/popcorn.conf'
 
-_config = RawConfigParser()
-_config.read(CONFIG_FILE)
-rdb = redis.Redis(_config.get('redis', 'hostname'),
-                  int(_config.get('redis', 'port')),
-                  int(_config.get('redis', 'db')))
+config = RawConfigParser()
+config.read(CONFIG_FILE)
+rdb = redis.Redis(config.get('redis', 'hostname'),
+                  int(config.get('redis', 'port')),
+                  int(config.get('redis', 'db')))
