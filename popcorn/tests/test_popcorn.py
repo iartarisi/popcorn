8f735427a536a35716ec6bd74efc75cc4682d512
Ionuț Arțăriși <iartarisi@suse.cz>
2011-09-29 13:18:50 +0200

    improve functional test with more assertions

--- popcorn/tests/test_popcorn.py
+++ popcorn/tests/test_popcorn.py
@@ -1,4 +1,3 @@
-from datetime import date
 import unittest
 
 import redis
@@ -10,15 +9,25 @@ from popcorn import server
 
 class TestPopcorn(unittest.TestCase):
     def test_submit_success(self):
-        submission = ("POPCORN 0.1 x86_64 a564f30e-1e15-4546-831f-b000608b9889\n"
+        submission = ("POPCORN 0.1 x86_64 TEST_HW_UUID\n"
                       "v python 2.5 1.1 None x86_64 openSUSE-11.4\n"
                       "o python-lint 1.1 1 None noarch openSUSE-11.4\n")
         server.parse_popcorn(submission)
         self.assertEqual(rdb.get('global:nextSubmissionId'), '1')
         self.assertEqual(rdb.get('global:nextSystemId'), '1')
         self.assertEqual(rdb.get('global:nextPackageId'), '2')
+        self.assertEqual(rdb.get(
+            'package:python:2.5:1.1:None:x86_64:openSUSE-11.4'), '1')
+        self.assertEqual(rdb.get(
+            'package:python-lint:1.1:1:None:noarch:openSUSE-11.4'), '2')
         self.assertEqual(rdb.hget('package:1:status', 'voted'), '1')
         self.assertEqual(rdb.hget('package:2:status', 'old'), '1')
+
+        self.assertEqual(rdb.smembers('vendors'), set(['openSUSE-11.4']))
+        self.assertEqual(rdb.smembers('vendor:openSUSE-11.4:packages'),
+                         set(['1', '2']))
+        self.assertEqual(rdb.smembers('systems'), set(['TEST_HW_UUID']))
+        self.assertEqual(rdb.smembers('system:1:submissions'), set(['1']))
 
     def tearDown(self):
         rdb.flushdb()
