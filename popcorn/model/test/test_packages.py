042fc2adfde15bbe1267ba3ec0c9de42b19dbc85
Ionuț Arțăriși <iartarisi@suse.cz>
2011-10-07 16:54:27 +0200

    extend Package model tests

--- popcorn/model/test/test_packages.py
+++ popcorn/model/test/test_packages.py
@@ -29,49 +29,76 @@ import redis
 from popcorn import configs
 configs.rdb = rdb = redis.Redis('localhost', db='13')
 from popcorn.model import Distro, Package, Vendor, System, Submission
+from popcorn.model.error import DoesNotExist
 
 
 class TestPackages(unittest.TestCase):
 
     def setUp(self):
         self.distro = Distro('Foo', '0.0')
+        self.sys = System('sysid', 'Foo', '0.0', 'i586')
+        self.vendor = Vendor('v')
+        self.sub = Submission(self.sys, 'popver')
 
     def test_init_package_attributes(self):
-        ven = Vendor('v')
-        sys = System('sysid', 'Foo', '0.0', 'i586')
-        sub = Submission(sys, 'popver')
-        p = Package('name', 'ver', 'rel', 'ep', 'arch', ven, 'o', sub)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'o', self.sub)
         self.assertTupleEqual(
-            (p.name, p.version, p.release, p.epoch, p.arch, p.vendor, p.status, p.sub.__class__),
-            ('name', 'ver', 'rel', 'ep', 'arch', ven, 'old', Submission))
+            (p.name, p.version, p.release, p.epoch, p.arch,
+             p.vendor, p.status, p.sub.__class__),
+            ('name', 'ver', 'rel', 'ep', 'arch',
+             self.vendor, 'old', Submission))
 
     def test_init_package_status_old(self):
-        vendor = Vendor('v')
-        sys = System('sysid', 'Foo', '0.0', 'i586')
-        Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'o', sys)
-        p = Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'o', sys)
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'o', self.sub)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'o', self.sub)
         self.assertEqual(p.old, '2')
 
     def test_init_package_status_recent(self):
-        vendor = Vendor('v')
-        sys = System('sysid', 'Foo', '0.0', 'i586')
-        Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'r', sys)
-        p = Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'r', sys)
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'r', self.sub)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'r', self.sub)
         self.assertEqual(p.recent, '2')
 
     def test_init_package_status_nofiles(self):
-        vendor = Vendor('v')
-        sys = System('sysid', 'Foo', '0.0', 'i586')
-        Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'n', sys)
-        p = Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'n', sys)
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'n', self.sub)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'n', self.sub)
         self.assertEqual(p.nofiles, '2')
 
     def test_init_package_status_voted(self):
-        vendor = Vendor('v')
-        sys = System('sysid', 'Foo', '0.0', 'i586')
-        Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'v', sys)
-        p = Package('name', 'ver', 'rel', 'ep', 'arch', vendor, 'v', sys)
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'v', self.sub)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'v', self.sub)
         self.assertEqual(p.voted, '2')
+
+    def test_init_package_already_exists_same_id(self):
+        p = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'v', self.sub)
+        sub2 = Submission(self.sys, 'popver')
+        r = Package('name', 'ver', 'rel', 'ep', 'arch',
+                    self.vendor, 'v', sub2)
+        self.assertEqual(p.id, r.id)
+        self.assertEqual(len(self.sub.packages), 1)
+        self.assertEqual(self.sub.packages.pop().id, '1')
+
+    def test_find_package(self):
+        package = Package('name', 'ver', 'rel', 'ep', 'arch',
+                          self.vendor, 'v', self.sub)
+        self.assertIsInstance(Package.find(1), Package)
+
+    def test_find_package_not_found(self):
+        self.assertRaises(DoesNotExist, Package.find, 1)
+
+    def test_status_on_system(self):
+        s1 = Submission(self.sys, 'popver')
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'v', s1)
+        s2 = Submission(self.sys, 'popver')
+        Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'r', s2)
+        p = Package('name', 'ver', 'rel', 'ep', 'arch', self.vendor, 'o', s2)
+        self.assertEqual(p.status_on_system(self.sys), 'old')
+
 
     def tearDown(self):
         rdb.flushdb()
