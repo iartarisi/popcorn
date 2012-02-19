a795760fd7fabbec22f806d095ce917fc19ca02e
Ionuț Arțăriși <iartarisi@suse.cz>
2012-02-19 17:02:33 +0100

    add a drop_db convenience function to popcorn.database

--- popcorn/database.py
+++ popcorn/database.py
@@ -34,9 +34,12 @@ db_session = scoped_session(sessionmaker(autocommit=False,
 Base = declarative_base()
 Base.query = db_session.query_property()
 
+# import all modules here that might define models so that
+# they will be registered properly on the metadata.
+import popcorn.models
+
 def init_db():
-    # import all modules here that might define models so that
-    # they will be registered properly on the metadata.  Otherwise
-    # you will have to import them first before calling init_db()
-    import popcorn.models
     Base.metadata.create_all(bind=engine)
+
+def drop_db():
+    Base.metadata.drop_all(bind=engine)
