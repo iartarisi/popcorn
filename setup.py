acae9581ec246c405b4745731338d05db651d4a3
Ionuț Arțăriși <mapleoin@lavabit.com>
2012-03-10 13:38:08 +0100

    fix setup.py - load models tests and add required metadata

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 
 setup(
     name="popcorn",
     version="0.1",
+    url="http://github.com/mapleoin/popcorn",
+    author="Ionuț Arțăriși",
+    author_email="mapleoin@lavabit.com",
     long_description=__doc__,
     packages=find_packages(exclude=["*.test", "test", "*.test.*"]),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['Flask', 'jinja2', 'redis'],
+    install_requires=['Flask', 'sqlalchemy', 'psycopg2'],
     test_suite="popcorn.test"
     )
