492d2974e7ac4bc3508767bb31dbfb9c42662818
Ionuț Arțăriși <iartarisi@suse.cz>
2012-03-23 10:56:15 +0100

    dotcloudify

--- popcorn/configs.py
+++ popcorn/configs.py
@@ -1,2 +1,8 @@
-DB_ENGINE = 'postgresql://popcorn:popcorn@localhost/popcorn'
+import json
+
+with open('/home/dotcloud/environment.json', 'r') as f:
+  conf = json.load(f)
+DB_ENGINE = 'postgresql://popcorn:popcorn@%(host)s:%(port)s/popcorn' % {
+  'host': conf['DOTCLOUD_DB_SQL_HOST'],
+  'port': conf['DOTCLOUD_DB_SQL_PORT']}
 SUBMISSION_INTERVAL = 30
---
+++ wsgi.py
@@ -0,0 +1,3 @@
+# this file is needed by dotcloud
+from popcorn import app as application
+application.config['DEBUG'] = True
