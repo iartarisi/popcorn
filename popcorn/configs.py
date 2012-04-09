1b6548eb75e17f6c49a3d64e4c42e467f66742ed
Gaurav Sood <gauravsood.91@gmail.com>
2012-04-09 16:46:43 +0530

    Config file edit

--- popcorn/configs.py
+++ popcorn/configs.py
@@ -2,10 +2,10 @@ DB_ENGINE = 'postgresql://popcorn:popcorn@localhost/popcorn'
 SUBMISSION_INTERVAL = 30
 
 #configuration settings for mail during traceback
-SMTP_SERVER='smtp.gmail.com'
+SMTP_SERVER=''
 SMTP_PORT=25
-HOST_EMAIL='gauravsood.91@gmail.com'
-HOST_PASS='ucertainlywontexpectthis'
-ADMIN_MAIL='gauravs@sitpune.edu.in'
+HOST_EMAIL=''
+HOST_PASS=''
+ADMIN_MAIL=''
 
 MESSAGE='From: '+HOST_EMAIL+'\n'+'To: '+ADMIN_MAIL+'\n'+'Subject: Traceback'+'\n\n'
